b461aa15915afe4abd08fed7c20678762888d1e9
Dominique Vieira <dominique.vieira1@gmail.com>
2023-12-13 16:56:24 -0300

    Terceira Alteração

--- hello.py
+++ hello.py
@@ -1,3 +1,28 @@
 #!
-#comentario
-print('Hello, World!')
+"""Hello World Multi Linguas.
+Denpendendo da lingua configurada no ambiente o programa exibe a mensagem cor
+respondente.
+
+Como usar:
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+#metadados 
+__version__ = '0.0.1'
+__autor__ = 'Magalhaes'
+__license__ = 'Unlicense'
+
+#dunder = __
+
+#Este Programa imprime Hello World
+
+msg = 'Hello, World!'
+
+print(msg)
