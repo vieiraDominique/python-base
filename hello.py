9b4bc491ca76af9825726fc59f813252bd9bec0a
Dominique Vieira <dominique.vieira1@gmail.com>
2023-12-19 10:57:07 -0300

    Realizado a implementacao do Hello

--- hello.py
+++ hello.py
@@ -15,14 +15,27 @@ Execução:
     ./hello.py
 """
 #metadados 
-__version__ = '0.0.1'
-__autor__ = 'Magalhaes'
-__license__ = 'Unlicense'
+__version__ = "0.0.1"
+__autor__ = "Magalhaes"
+__license__ = "Unlicense"
+
+import os
 
 #dunder = __
 
 #Este Programa imprime Hello World
+current_language = os.getenv("LANG")[:5]
+#snake_case
 
-msg = 'Hello, World!'
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+     msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+     msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+     msg = "Bonjour Monde"
 
 print(msg)
